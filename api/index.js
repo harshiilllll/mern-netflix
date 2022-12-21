4fd72a0dccb5b210bab67757b63d95b3549722ee
Harshil Gajjar <91361662+harshiilllll@users.noreply.github.com>
2022-12-21 11:28:47 +0000

    api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const authRoute = require("./routes/auth");
+const app = express();
+const PORT = 8000 || process.env.PORT;
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+.then(() => console.log("MONGO DB CONNECTED"))
+.catch((err) => console.log(err))
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+
+app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
+});
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './app.scss';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
