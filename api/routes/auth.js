099662c47cf9dd5015c3e4b95f1274e03d520793
harshil <harshilgajjar54@gmail.com>
2022-12-22 14:13:39 +0530

    auth err

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -20,7 +20,7 @@ router.post("/register", async (req, res) => {
 })
 
 //LOGIN
-router.post("login", async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email});
         !user && res.status(401).json("Wrong username or password");
